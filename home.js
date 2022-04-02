a28e11305c8e66f3dd28df8e5cbd467d671339fa
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-02 14:03:26 +0530

    For loop and while loop

--- home.js
+++ home.js
@@ -1,16 +1,27 @@
 // var age = prompt("What's your age? ");
 // document.getElementById('age').innerHTML = age;
 
-function fun(){
-  var name = prompt("Please telll me your name? ");
-  console.log("hello " + name);
+// function fun(name){
+//   console.log("hello " + name);
+// }
+
+// var name = prompt("Please telll me your name? ");
+// fun(name);
+
+
+// function sumNum(num1, num2){
+//   console.log(num1+num2);
+// }
+
+// sumNum(20,30);
+
+var num =1;
+while(num <=100){
+  console.log(num);
+  num++;
 }
 
-fun();
 
-
-function sumNum(num1, num2){
-  console.log(num1+num2);
+for(let num1 = 1; num1<=10; num1++){
+  console.log(num1);
 }
-
-sumNum(20,30);
