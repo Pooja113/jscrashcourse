35c05f80fc58a6e5a78ab559dc62aac603d12d4d
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-02 14:15:51 +0530

    string operations

--- home.js
+++ home.js
@@ -15,13 +15,29 @@
 
 // sumNum(20,30);
 
-var num =1;
-while(num <=100){
-  console.log(num);
-  num++;
-}
+// var num =1;
+// while(num <=100){
+//   console.log(num);
+//   num++;
+// }
 
 
-for(let num1 = 1; num1<=10; num1++){
-  console.log(num1);
-}
+// for(let num1 = 1; num1<=10; num1++){
+//   console.log(num1);
+// }
+
+let random //undefined
+let nothing = null // value null
+
+
+let fruit = 'mango';
+let moreFruits = 'mango\nbanana'; //new line
+console.log(moreFruits);
+
+console.log(fruit.length); //string length
+console.log(fruit.indexOf('q')); 
+console.log(fruit.slice(2,5));
+console.log(fruit.replace('man','123'));
+console.log(fruit.toUpperCase());
+console.log(fruit.charAt(2));
+console.log(fruit.split(''));
