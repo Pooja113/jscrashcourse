0a246127466098ee63c11426593079b7e58672f6
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-02 13:58:26 +0530

    functions

--- home.js
+++ home.js
@@ -1,2 +1,16 @@
-var age = prompt("What's your age? ");
-document.getElementById('age').innerHTML = age;
+// var age = prompt("What's your age? ");
+// document.getElementById('age').innerHTML = age;
+
+function fun(){
+  var name = prompt("Please telll me your name? ");
+  console.log("hello " + name);
+}
+
+fun();
+
+
+function sumNum(num1, num2){
+  console.log(num1+num2);
+}
+
+sumNum(20,30);
