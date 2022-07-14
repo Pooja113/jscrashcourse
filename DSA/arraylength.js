42c196be0802dcd5660805744a3962a74e686251
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-07-14 23:18:30 +0530

    Add files via upload

--- DSA/arraylength.js
+++ DSA/arraylength.js
@@ -1,14 +1,35 @@
-let nums = [2,7,11,15,2,2,'4'];
+let nums = [2,7,11];
 
-function arrlen(nums){
-    let count = 0;
-    while (nums[count] !== undefined) {
-        count++
-    }
-    return count;
-}
+// function arrlen(nums){
+//     let count = 0;
+//     while (nums[count] !== undefined) {
+//         count++
+//     }
+//     return count;
+// }
 
-console.log(arrlen(nums))
+// console.log(arrlen(nums))
 
 
+// Multiple ways to print array
+
+//    for (let index = 0; index < nums.length; index++) {
+//         console.log(nums[index]);
+//    }
+
+// nums.forEach((element,index) => {
+//     console.log(element)
+//    });
+
+
+//    for(let el in nums){
+//     console.log(nums[el]);
+//    }
+
+   for(let el of nums){
+    console.log(el);
+   }
+   
+
+   
 
